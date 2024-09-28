7d27f0db6280fbade2bd6c2941fb2efc2585e8ac
Ahmed Atigui <ahmedatigui001@gmail.com>
2024-09-28 21:22:44 +0100

    fx: type bug

--- client/socket.ts
+++ client/socket.ts
@@ -1,8 +1,8 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:6969/';
+const URL = process.env.NODE_ENV === 'production' ? process.env.SOCKET_URL : 'http://localhost:6969/';
 
-export const socket = io(URL, {
+export const socket = io(URL!, {
   autoConnect: false
 });
